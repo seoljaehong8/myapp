1d532559d248321f217730a3ea67eb977506de84
jaehong <seol940601@gmail.com>
2021-10-13 21:27:42 +0900

    update

--- mypjt/mypjt/urls.py
+++ mypjt/mypjt/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/myapp/', include('myapp.urls'))
+    path('myapp/', include('myapp.urls'))
 ]
